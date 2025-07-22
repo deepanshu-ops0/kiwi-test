ebf9ae114a65e9b1832d3f7d2515ad2be344271d
deepanshu-ops0 <deepanshu.yadav@ops0.com>
2025-07-22 22:23:28 +0530

    IaC changes

---
+++ e7839491-df1a-4e6a-a76f-1dd993217934/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+
+  # Prevent public access
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Enable versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
